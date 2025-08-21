7951c19e51581126b36795cdbf19cbf7d06c62df
Op-harsh <gamer6203788@gmail.com>
2025-08-21 23:55:05 +0530

    Update stream.py

--- core/stream.py
+++ core/stream.py
@@ -28,7 +28,8 @@ from pytgcalls.types.stream import MediaStream
 from pyrogram.raw.types import InputPeerChannel
 from pytgcalls.types import AudioQuality, VideoQuality
 from pyrogram.raw.functions.phone import CreateGroupCall
-from pytgcalls.exceptions import GroupCallNotFound, NoActiveGroupCall
+from py_tgcalls.exceptions import GroupCallNotFound, NoActiveGroupCall
+
 
 
 safone = {}
